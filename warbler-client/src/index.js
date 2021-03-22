00bdf168e434f3456403da7f76596c7078e82c94
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-03-22 15:30:21 +0000

    saved index.js file

--- warbler-client/src/index.js
+++ warbler-client/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+// import App from './App';
+// can't find App compiled fail 
+// so use "./containers/App";
 import App from "./containers/App";
 import registerServiceWorker from "./registerServiceWorker";
 
